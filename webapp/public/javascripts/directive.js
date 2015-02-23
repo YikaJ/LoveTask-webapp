b1267008aa24c2306b549062568b0f10d7cba191
YikaJ <547179699@qq.com>
2015-02-23 23:31:13 +0800

    first version done

--- webapp/public/javascripts/directive.js
+++ webapp/public/javascripts/directive.js
@@ -26,11 +26,10 @@ directiveModule.directive('loginForm', function($http, PostTask){
 });
 
 //增加任务
-directiveModule.directive('addTask', function(){
+directiveModule.directive('addTask', function($timeout){
   return {
     restrict: 'EA',
     replace: true,
-    required: 'ngModel',
     templateUrl: './addTask.html',
     controller: function($scope, $state, $stateParams, PostTask){
       $scope.newTask = {};
@@ -39,8 +38,7 @@ directiveModule.directive('addTask', function(){
         var newTask = angular.copy($scope.newTask);
         $scope.newTask = {};//重置
         newTask.status = 0;
-        var date = newTask.limitTime.split('-');
-        newTask.limitTime = new Date(date[0], date[1]-1, date[2]);
+        newTask.limitTime = getDate(newTask.limitTime);
         $scope.task[$scope.task[$stateParams.type].loverType].taskList.unshift(newTask);
 
         PostTask.saveTask('/saveTask', $scope.task)
@@ -50,10 +48,29 @@ directiveModule.directive('addTask', function(){
       };
     },
     link: function(scope, element, attr){
-      $(element).find('input[name=limitTime]').keyup(function(){
-        //console.log($(this).val());
-        this.value = this.value.replace(/[^0-9\-]/, "");
-      });
+      $(element).find('input[name=limitTime]')
+        .keyup(function(){
+          var value = this.value = this.value.replace(/[^0-9\-]/, "");
+          var timer;
+          if(timer){
+            $timeout.cancel(timer);
+          }else{
+            timer = $timeout(function(){
+              if(getDate(value) <= new Date()){
+                console.log('时间不准确！');
+                scope.addTaskForm.limitTime.$setValidity('validTime', false);
+              }else{
+                scope.addTaskForm.limitTime.$setValidity('validTime', true);
+              }
+            }, 350)
+          }
+        })
     }
   }
 });
+
+
+function getDate(dateStr){
+  var date = dateStr.split('-');
+  return new Date(date[0], date[1]-1, date[2]);
+}
